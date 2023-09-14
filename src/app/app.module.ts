cf4f17c95c0d5f2b02b17b006a5724286ab7e3d1
Tom Williams <t0mwilli4ms@gmail.com>
2023-09-14 14:39:07 +0100

    feat: Added basic signal testing before attempting to add CrComLib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BasicSignalsComponent } from './basic-signals/basic-signals.component';
+import { SliderElementComponent } from './slider-element/slider-element.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicSignalsComponent,
+    SliderElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
