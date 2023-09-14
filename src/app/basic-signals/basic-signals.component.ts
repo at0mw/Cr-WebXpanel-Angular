10fde358a361e01bf49fb8951dce910c78554382
Tom Williams <t0mwilli4ms@gmail.com>
2023-09-14 15:19:05 +0100

    feat: Linked up buttons.

--- src/app/basic-signals/basic-signals.component.ts
+++ src/app/basic-signals/basic-signals.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from '../services/message.service'
 
 @Component({
   selector: 'app-basic-signals',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-signals.component.scss']
 })
 export class BasicSignalsComponent {
+  constructor(private messageService: MessageService) {}
+  inputValue: string = '';
+  messageReceived: string = '';
 
+  buttonPressed(buttonId: number): void {
+    console.log("Button Pressed...");
+    console.log("Button Pressed :: Destination : ", buttonId);
+    this.messageService.sendActionMessage(buttonId.toString());
+  }
+
+  handleTextInput() {
+    // Handle the text input changes
+    console.log(`Input Value: ${this.inputValue}`);
+    // You can perform any actions you need with the input value.
+    this.messageReceived = this.inputValue;
+  }
 }
